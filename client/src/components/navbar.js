4255327be23b29455bc46137b96d0f7290cbf5aa
karan-patel61 <karanpatel_work@hotmail.com>
2022-09-21 12:49:19 -0400

    navbar fix

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,6 +2,8 @@ import React from "react";
  
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
+
+import "bootstrap/dist/js/bootstrap.bundle";
  
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
@@ -9,32 +11,26 @@ import { NavLink } from "react-router-dom";
 // Here, we display our Navbar
 export default function Navbar() {
  return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <NavLink className="navbar-brand" to="/"> Nav Link
-       </NavLink>
-       <button
-         className="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Create Record
-             </NavLink>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   </div>
+  <div className="container-fluid">
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <NavLink className="navbar-brand" to="/">
+    <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+    </NavLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0"> */}
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/create">
+            Create Record
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
  );
 }
