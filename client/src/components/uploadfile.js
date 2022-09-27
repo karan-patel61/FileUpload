dea3c6af9d31bab57ba7fbdeafdc59028be415ac
karan-patel61 <karanpatel_work@hotmail.com>
2022-09-27 19:02:17 -0400

    succesfully configured post command on upload file componenet

--- client/src/components/uploadfile.js
+++ client/src/components/uploadfile.js
@@ -1,54 +1,79 @@
 import { Button } from "bootstrap/dist/js/bootstrap.bundle";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import {useForm} from "react-hook-form";
 import { useParams, useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 
 export default function UploadFile() {
-  const [uploads, setForm] = useState({
-    originalname: "",
-    size: "",
-    type: "",
-  });
-  
-  // These methods will update the state properties.
- function updateForm(value) {
+const navigate = useNavigate();
+const [selectedFile, setSelectedFile] = useState();
+const [isFilePicked, setIsFilePicked] = useState(false);
+const changeHandler = (event) => {
+  setSelectedFile(event.target.files[0]);
+  setIsFilePicked(true);
+  form.name = event.target.files[0].name;
+  form.size = event.target.files[0].size;
+  form.type = event.target.files[0].type;
+  console.log("Selected File : "+selectedFile);
+};
+
+const [form, setForm] = useState({
+  name: "",
+  size: "",
+  type: "",
+});
+// These methods will update the state properties.
+function updateForm(value) {
   return setForm((prev) => {
     return { ...prev, ...value };
   });
 }
+const handleSubmission = async () => {
   
-  // This function will handle the submission.
- async function onSubmit(e) {
-    e.preventDefault();
   
-    // When a post request is sent to the create url, we'll add a new record to the database.
-    //const newFile = { ...uploads };
-    const newFile = document.getElementById('formFile').fileList[0];
-    await fetch("http://localhost:5000//uploadfile/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newFile),
-    })
-    .catch(error => {
-      window.alert(error);
-      return;
-    });
-    newFile.innerHTML = "";
-    setForm({ originalname: "", size: "", type: "" });
-    
-  }
+  const formData = {...form};
+	//formData.append('File', f);
+  await fetch("http://localhost:5000/uploadfile/add", {
+     method: "POST",
+     headers: {
+      "Content-Type": "application/json",
+    },
+     body: JSON.stringify(formData),
+   })
+   .catch(error => {
+     window.alert(error);
+     return;
+   });
+   navigate("/");
+
+};
+
     // This following section will display the form that takes input from the user to update the data.
     return (
         <div>
             <h3>Uploaded Files</h3>
-            <div class="mb-3">
-                <label for="formFile" class="form-label">Default file input example</label>
-                <input class="form-control" type="file" id="formFile"/>
-                <input type="Submit" onSubmit={onSubmit}/> 
-            </div>
-            
+            <form>
+              <input 
+              type="file"
+              id="formFile"
+              className="form-control"
+              onChange={changeHandler}
+              />
+              <button onClick={handleSubmission}>Submit</button>
+              {isFilePicked ? (
+              <div>
+                <p >Filename: {selectedFile.name }</p>
+                <p >Filetype: {selectedFile.type}</p>
+                <p >Size in bytes: {selectedFile.size}</p>
+                <p>
+                  lastModifiedDate:{' '}
+                  {selectedFile.lastModifiedDate.toLocaleDateString()}
+                </p>
+              </div>
+			        ) : (
+				      <p>Select a file to show details</p>
+			        )}
+            </form>
         </div>
     );
    }
