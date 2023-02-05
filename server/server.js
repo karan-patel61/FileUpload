b8651222cf2386e6a5b7caf854dda76be3ef6242
karan-patel61 <patel_karan_cool@hotmail.com>
2023-02-05 11:37:24 -0500

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.listen(port, () => {
  //My change made on this line
 });
 // app.get('/', (req, res) => { res.send('Hello from Express!'+" On Port: "+port)});
-app.use(express.static(path.join(__dirname, '../client')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client')) 
-});
+// app.use(express.static(path.join(__dirname, '../client')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../')) 
+// });
