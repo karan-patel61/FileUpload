9339684c04f90bfe291e32b29ffcf9904a9d15a5
karan-patel61 <patel_karan_cool@hotmail.com>
2023-02-05 12:01:56 -0500

    clean up server.js

--- server/server.js
+++ server/server.js
@@ -18,10 +18,5 @@ app.listen(port, () => {
  
   });
   console.log(`Server is running on port: ${port}`);
- //My change made on this line
+  
 });
- app.get('/', (req, res) => { res.send('Hello from Express!'+" On Port: "+port)});
-// app.use(express.static(path.join(__dirname, '../client')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../')) 
-// });
